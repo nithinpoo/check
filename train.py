9e02a2dbe7b64592afd496b25282a658ec26239f
RajAj01 <106050418+RajAj01@users.noreply.github.com>
2024-04-07 12:37:15 -0700

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 # Introduce an error by providing an incorrect file path
-df = pd.read_csv("data/train.csv")  # correct file path
+df = pd.read_csv("dataaa/train.csv")  # correct file path
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
